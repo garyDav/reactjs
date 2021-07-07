ffd97f65ccd352072659be48b543d7546b513a6a
garyDav <gary.2810.dav@gmail.com>
2021-07-07 08:09:25 -0400

    05-pruebas finish

--- 05-pruebas/counter-app/src/tests/CounterApp.test.js
+++ 05-pruebas/counter-app/src/tests/CounterApp.test.js
@@ -1,3 +1,7 @@
+import '@testing-library/jest-dom'
+import { shallow } from 'enzyme'
+import testUtils from 'react-dom/test-utils'
+import CounterApp from '../CounterApp'
 /***
   #Tarea
     1. Crear las siguientes pruebas para el <CounterApp />
@@ -6,8 +10,48 @@
         * Usar el wrapper.find, tomando el elemento html donde se muestra el valor del contador
 ***/
 describe('Pruebas de mi componente <CounterApp />', () => {
+  // let wrapper = shallow( <CounterApp /> )
+  let wrapper
+
+  beforeEach( () => {
+    wrapper = shallow( <CounterApp /> )
+  })
+
   test('Debe de mostrar <CounterApp /> correctamente, y sus valores por defecto', () => {
-    // Go
+
+    expect( wrapper ).toMatchSnapshot()
 
   })
+
+  test('Debe de mostrar el valor por defecto de 100', () => {
+    const wrapper = shallow( <CounterApp value={ 100 } /> )
+
+    const counterText = wrapper.find('h2').text().trim()
+    expect( counterText ).toBe('100')
+  })
+
+  test('Debe de incrementar con el botón +1', () => {
+    wrapper.find('button').at(0).simulate('click')
+
+    const counterText = wrapper.find('h2').text().trim()
+    expect( counterText ).toBe('11')
+  })
+
+  test('Debe de decrementar con el botón -1', () => {
+    wrapper.find('button').at(2).simulate('click')
+
+    const counterText = wrapper.find('h2').text().trim()
+    expect( counterText ).toBe('9')
+  })
+
+  test('Debe de colocar el valor por defecto con el btn reset', () => {
+    const wrapper = shallow( <CounterApp value={105} /> )
+    wrapper.find('button').at(0).simulate('click')
+    wrapper.find('button').at(0).simulate('click')
+    wrapper.find('button').at(1).simulate('click')
+    const counterText = wrapper.find('h2').text().trim()
+
+    expect( counterText ).toBe('105')
+  })
+
 })
